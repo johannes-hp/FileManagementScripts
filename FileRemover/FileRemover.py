03a908250f8064707e0fc43f2a69743194dc7e64
Johannes Hermann Palbøl <johanneshp@hotmail.com>
2021-11-04 14:26:30 +0100

    Add files via upload

---
+++ FileRemover/FileRemover.py
@@ -0,0 +1,19 @@
+def file_remover():
+    import os
+    import pathlib
+    basepath = pathlib.Path(__file__).parent.resolve() #finds current directory
+
+    for entry in os.listdir(basepath): #loops over subentries in cd
+        if os.path.isdir(os.path.join(basepath, entry)): #checks if entry is a directory
+            for file in os.listdir(str(basepath) + "\\" + entry): #loops over files in subdirectory
+                if file.lower().endswith('tif') or file.lower().endswith('zip') or file.lower().endswith('jpg') or file.lower().endswith('tmp'):
+                    continue #skips files with the file extensions in the if-statement
+                else:
+                    print(file, 'deleted') #prints to terminal
+                    os.remove(os.path.join(basepath, entry, file)) #deletes file
+                    with open('FileRemoverOutput.txt', 'a') as fout: #opens output file
+                        fout.write(file + ' deleted\n') #writes to output file
+
+    return
+            
+file_remover()
