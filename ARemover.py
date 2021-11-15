39a578f04645891555386055c12943ab6fa6a531
Johannes Hermann Palbøl <johanneshp@hotmail.com>
2021-11-15 14:01:40 +0100

    Update ARemover.py

--- ARemover.py
+++ ARemover.py
@@ -5,11 +5,14 @@ def ARemover():
 
     for entry in os.listdir(basepath): #loops over subentries in cd
         if os.path.isdir(os.path.join(basepath, entry)): #checks if entry is a directory
-            for file in os.listdir(str(basepath) + "\\" + entry): #loops over files in subdirectory
-                if "a." in file.lower():
-                    print(file, 'deleted') #prints to terminal
-                    os.remove(os.path.join(basepath, entry, file)) #deletes file
-                    with open('ARemoverOutput.txt', 'a') as fout: #opens output file
-                        fout.write(file + ' deleted\n') #writes to output file
+            if "RECYCLE" in os.path.join(basepath, entry):
+                continue
+            else:
+                for file in os.listdir(str(basepath) + "\\" + entry): #loops over files in subdirectory
+                    if "a." in file.lower():
+                        print(file, 'deleted') #prints to terminal
+                        os.remove(os.path.join(basepath, entry, file)) #deletes file
+                        with open('ARemoverOutput.txt', 'a') as fout: #opens output file
+                            fout.write(os.path.join(basepath, entry, file) + ' deleted\n') #writes to output file
 
 ARemover()
