cc0e105c927305643fa4a3b298290134025c49da
Johannes Hermann Palbøl <johanneshp@hotmail.com>
2021-11-15 14:09:32 +0100

    Update FileRemover.py

--- FileRemover.py
+++ FileRemover.py
@@ -9,7 +9,7 @@ def file_remover():
                 continue
             else:
                 for file in os.listdir(str(basepath) + "\\" + entry): #loops over files in subdirectory
-                    if file.lower().endswith(('tif', 'tif_original', 'tiff', 'zip', 'jpg', 'jpeg', 'tmp')):
+                    if file.lower().endswith(('tif', 'tif_original', 'tiff', 'zip', 'jpg', 'jpeg', 'tmp', 'txt')):
                         continue #skips files with the file extensions in the if-statement
                     else:
                         print(file, 'deleted') #prints to terminal
